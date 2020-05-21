31eb20abb223b68ef2d5c802ea13181417cfcd57
Jonathan Gayfield <onlyperception@JonathaeldsMBP2.cable.rcn.com>
2020-05-20 23:09:06 -0400

    solved

--- introduction_and_environment/hello_world/3_user_input/solution/solution.py
+++ introduction_and_environment/hello_world/3_user_input/solution/solution.py
@@ -1,1 +1,7 @@
 # Code your solution here
+print("Please input three different numbers separated by return: ")
+FIRST_NUM = float(input())
+SECOND_NUM = float(input())
+THIRD_NUM = float(input())
+
+print((FIRST_NUM + SECOND_NUM + THIRD_NUM) / 3)
