077fc49db31ee2a20e27f0211463229bd194e5ad
Jonathan Gayfield <onlyperception@JonathaeldsMBP2.cable.rcn.com>
2020-05-20 23:54:34 -0400

    solved

--- introduction_and_environment/git/2_python_caches/solution/solution.py
+++ introduction_and_environment/git/2_python_caches/solution/solution.py
@@ -1,1 +1,26 @@
+x = 1
+y = 1
 
+while True:
+    x += 1
+    y += 1
+    if id(x) == id(y):
+        pass
+    else:
+        print(x, y)
+        break
+
+x = 1
+y = 1
+
+while True:
+    x -= 1
+    y -= 1
+    if id(x) == id(y):
+        pass
+    else:
+        print(x, y)
+        break
+
+LOWER_BOUND = -5
+UPPER_BOUND = 256
