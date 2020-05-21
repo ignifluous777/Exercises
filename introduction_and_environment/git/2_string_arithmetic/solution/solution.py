5e61f36c78eb95b50983c35e3255700ccbcdcdc4
Jonathan Gayfield <onlyperception@JonathaeldsMBP2.cable.rcn.com>
2020-05-21 00:22:50 -0400

    solved

--- introduction_and_environment/git/2_string_arithmetic/solution/solution.py
+++ introduction_and_environment/git/2_string_arithmetic/solution/solution.py
@@ -1,1 +1,77 @@
 # Code your solution here
+STR_ARITHMETIC = 0
+s = "string"
+n = 2
+
+try:
+    print(s + s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s - s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s * s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s / s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s ** s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s // s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s % s)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+
+try:
+    print(s + n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s - n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s * n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s / n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s ** n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s // n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+try:
+    print(s % n)
+    STR_ARITHMETIC += 1
+except TypeError:
+    pass
+print(STR_ARITHMETIC)
