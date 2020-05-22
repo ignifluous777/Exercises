54434eb103f58fa5e2ae6cd9ace60af1fa6fc518
Jonathan Gayfield <onlyperception@JonathaeldsMBP2.cable.rcn.com>
2020-05-22 13:38:03 -0400

    solved

--- introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
+++ introduction_and_environment/data_types_and_control_flow/3_shape_check/Solution/solution.py
@@ -1,1 +1,9 @@
+print("Enter the sides of the shape you want from 3-9: ")
+figure = input()
+shape = {"3": "Triangle", "4":"Quadrilateral", "5": "pentagon", "6": "hexagon", "7": "heptagon", "8": "Octigone", "9": "Nonagon"}
+
+data = shape[figure]
+print(data)
+
+
 
